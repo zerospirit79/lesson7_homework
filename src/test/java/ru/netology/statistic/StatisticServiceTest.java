84398989905e754b1ff441f397711c308c4db276
Pavel Shilov <pavel.s.shilov@gmail.com>
2021-10-27 16:04:58 +0300

    update to 100%

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -14,4 +14,15 @@ class StatisticServiceTest {
 
             assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticService service = new StatisticService();
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 10, 9, 3};
+        long expected = 10;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+
 }
