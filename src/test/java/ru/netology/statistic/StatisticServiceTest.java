bd1d1b9919ca1f0d9d4e1917353456658dd2ef39
Pavel Shilov <pavel.s.shilov@gmail.com>
2021-10-27 15:39:48 +0300

    update pom

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -1,12 +1,11 @@
 package ru.netology.statistic;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticServiceTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findMax() {
-        void findMax() {
             StatisticService service = new StatisticService();
 
             long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
