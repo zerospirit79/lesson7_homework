46ac8cf6cdd996ad99c403bd58311cc94025c818
Pavel Shilov <pavel.s.shilov@gmail.com>
2021-10-27 15:34:36 +0300

    initial commit

---
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.statistic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void findMax() {
+        void findMax() {
+            StatisticService service = new StatisticService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+            long expected = 12;
+            long actual = service.findMax(incomesInBillions);
+
+            assertEquals(expected, actual);
+    }
+}
